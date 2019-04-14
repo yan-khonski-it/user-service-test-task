9d3cdba1551748ee28a0fc99da55bdbb2cde1489
yan-khonski-it <yankhonskiy@yandex.ru>
2019-04-14 17:30:50 +0200

    Initial commit

---
+++ src/main/java/com/yk/training/test/task/userservice/app/UserServiceApplication.java
@@ -0,0 +1,8 @@
+package com.yk.training.test.task.userservice.app;
+
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Running");
+    }
+}
